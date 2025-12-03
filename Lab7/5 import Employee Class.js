31ea02e3c1d5cef4418a9109ad880985b3f4bbc6
AhmedAshour88 <ahmed.elsayed.ashour@gmail.com>
2025-12-03 11:41:59 +0200

    Add files via upload

---
+++ Lab7/5 import Employee Class.js
@@ -0,0 +1,7 @@
+import { Employee } from "./employee.js";
+
+const emp1 = new Employee("Ahmed", "FullStack Developer");
+const emp2 = new Employee("Sara", "FrontEnd Developer");
+
+console.log(emp1.showDetails());
+console.log(emp2.showDetails());
